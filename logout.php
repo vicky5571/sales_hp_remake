4ad2d75aad3bd04d828dee9d7deed0a13ac19316
vicky5571 <vickygp5571@gmail.com>
2024-12-21 09:18:23 +0700

    deleted cart when logout (also deleting cart_items, and set added_to_cart value back to 0

--- logout.php
+++ logout.php
@@ -2,6 +2,16 @@
 session_start();
 session_destroy();
 
+include_once("conn.php");
+
+$conn -> query("SET foreign_key_checks = 0");
+$conn -> query("DELETE FROM carts");
+$conn -> query("UPDATE product_unit
+         SET added_to_cart = 0
+         WHERE imei IN (SELECT imei FROM cart_items)");
+$conn -> query("DELETE FROM cart_items");
+$conn -> query("SET foreign_key_checks = 1");
+
 // Redirect to the login page
 header('Location: login.php');
 exit;
